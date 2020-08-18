dd4cde59e259804ae9c7cadc107fbe05854e3536
Aruto Takano <sufumado1109@gmail.com>
2020-08-18 15:39:07 +0900

    20200818

--- main.js
+++
@@ -1,7 +0,0 @@
-function hamburger() {
-    document.getElementById('global-nav').classList.toggle('nav-open');
-}
-
-document.getElementById('ham').addEventListener('click', function () {
-　　hamburger();
-});
